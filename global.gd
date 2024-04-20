667187e62654873548da9064e265c8841ad79cfb
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2024-04-20 18:28:07 +0800

    Additional feature completion

--- global.gd
+++ global.gd
@@ -1,10 +1,23 @@
 extends Node
 
 var current_scene = null
+var total_score = 0
+var wrong_attempts = 0
+var correct_attempts = 0
+var max_score = 9
 
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
+
+func _set_total_score(score):
+	total_score = score
+	
+func _set_wrong_attemps(score):
+	wrong_attempts = score
+ 
+func _set_correct_attempts(score):
+	correct_attempts = score
 
 func goto_scene(path):
 	# This function will usually be called from a signal callback,
